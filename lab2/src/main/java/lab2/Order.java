e7a1c04fa0d35fa46bf9fee12ea1110a0fe455ee
StefanSebastian <stefansebii@gmail.com>
2017-10-17 11:49:33 +0300

    Added the new project for lab2

---
+++ lab2/src/main/java/lab2/Order.java
@@ -0,0 +1,7 @@
+package lab2;
+
+/**
+ * Created by Sebi on 17-Oct-17.
+ */
+public class Order {
+}
---
+++ lab2/src/main/java/lab2/benchmarkTests/RepositoryTest.java
@@ -0,0 +1,39 @@
+package lab2.benchmarkTests;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Sebi on 17-Oct-17.
+ */
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Benchmark)
+public class RepositoryTest {
+    @Param({"1", "31", "65", "101", "103", "1024", "10240", "65535"})
+    public int size;
+
+    @Benchmark
+    public void test(){
+        System.out.println("Running test");
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(RepositoryTest.class.getSimpleName())
+//                .addProfiler(HotspotMemoryProfiler.class)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
