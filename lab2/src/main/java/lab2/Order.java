f499f3039f93c8397745b4f84bf65b1e7ea5f040
StefanSebastian <stefansebii@gmail.com>
2017-10-21 16:24:25 +0300

    Added Order class

--- lab2/src/main/java/lab2/Order.java
+++ lab2/src/main/java/lab2/Order.java
@@ -3,5 +3,76 @@ package lab2;
 /**
  * Created by Sebi on 17-Oct-17.
  */
-public class Order {
+public class Order implements Comparable<Order> {
+    private int id;
+    private int price;
+    private int quantity;
+
+    public Order(int id, int price, int quantity) {
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        return quantity == order.quantity;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + price;
+        result = 31 * result + quantity;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        if (id < o.getId()){
+            return -1;
+        }
+        return id == o.getId() ? 0 : 1;
+    }
 }
