9208a1bd5769df9dca3aaf27075a375f6964ddbe
StefanSebastian <stefansebii@gmail.com>
2017-10-24 19:04:13 +0300

    Added all operations and plots

---
+++ lab2/charts/plotter/plotter.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
+
+headers = ['Benchmark','Supplier','Size','Score']
+df = pd.read_csv('data.csv',names=headers)
+
+i = 0
+for benchmark, df_benchmark in df.groupby('Benchmark'):
+    for size, df_size in df_benchmark.groupby('Size'):
+        print(df_size)
+        plt.bar(df_size['Supplier'], df_size['Score'])
+
+        benchmark_patch = mpatches.Patch(label=benchmark)
+        size_patch = mpatches.Patch(label=size)
+        plt.legend(handles=[benchmark_patch, size_patch])
+
+        i += 1
+
+        # rotate labels
+        plt.gcf().subplots_adjust(bottom=0.35)
+        plt.xticks(rotation=90)
+        plt.savefig(str(i))
+        #plt.show()
+        plt.clf()
--- lab2/src/main/java/lab2/RepositorySupplier.java
+++ lab2/src/main/java/lab2/RepositorySupplier.java
@@ -1,5 +1,13 @@
 package lab2;
 
+import com.koloboke.collect.map.hash.HashObjObjMaps;
+import com.koloboke.collect.set.hash.HashObjSet;
+import com.koloboke.collect.set.hash.HashObjSets;
+import gnu.trove.list.linked.TLinkedList;
+import gnu.trove.map.hash.TCustomHashMap;
+import gnu.trove.set.hash.THashSet;
+import gnu.trove.set.hash.TLinkedHashSet;
+import it.unimi.dsi.fastutil.objects.*;
 import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.factory.Maps;
 import org.eclipse.collections.impl.factory.Sets;
@@ -12,7 +20,6 @@ import java.util.function.Supplier;
  * Created by Sebi on 24-Oct-17.
  */
 public enum RepositorySupplier implements Supplier<InMemoryRepository<Order>> {
-
     HASH_SET(){
         @Override
         public InMemoryRepository<Order> get(){
@@ -50,7 +57,6 @@ public enum RepositorySupplier implements Supplier<InMemoryRepository<Order>> {
         }
     },
 
-    /*
     // eclipse collections
     ECLIPSE_MUTABLE_LIST(){
         @Override
@@ -71,7 +77,63 @@ public enum RepositorySupplier implements Supplier<InMemoryRepository<Order>> {
         public InMemoryRepository<Order> get() {
             return new CollectionRepository<>(() -> Collections.newSetFromMap(Maps.mutable.empty()));
         }
-    }
-*/
+    },
+
     // Koloboke
+    KOLOBOKE_HASH_SET(){
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(HashObjSets::newMutableSet);
+        }
+    },
+
+    KOLOBOKE_HASH_MAP(){
+      @Override
+        public InMemoryRepository<Order> get() {
+          return new CollectionRepository<>(
+                  () -> Collections.newSetFromMap(HashObjObjMaps.newMutableMap()));
+      }
+    },
+
+    TROVE_HASH_SET() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(THashSet::new);
+        }
+    },
+
+    TROVE_LINKED_HASH_SET() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(TLinkedHashSet::new);
+        }
+    },
+
+    FASTUTILS_ARRAY_LIST() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(ObjectArrayList::new);
+        }
+    },
+
+    FASTUTILS_ARRAY_SET() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(ObjectArraySet::new);
+        }
+    },
+
+    FASTUTILS_AVL_SET() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(ObjectAVLTreeSet::new);
+        }
+    },
+
+    FASTUTILS_RTB_SET() {
+        @Override
+        public InMemoryRepository<Order> get() {
+            return new CollectionRepository<>(ObjectRBTreeSet::new);
+        }
+    }
 }
